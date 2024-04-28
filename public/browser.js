a4679cd75b2c5b22f6ddf0127d9279923f200272
musan <133565834+themurugesan@users.noreply.github.com>
2024-04-28 19:13:51 +0530

    Add files via upload

---
+++ public/browser.js
@@ -0,0 +1,24 @@
+document.addEventListener("click",function(e){
+    //update
+    if(e.target.classList.contains("edit-me")){
+        let res=prompt("enter the new value")
+        
+        axios.post('/update-item',{text:res,id:e.target.getAttribute("data-id")}).then(function(){
+            
+            e.target.parentElement.parentElement.querySelector(".item-text").innerHTML=res
+        }).catch(function(){
+            console.log("error,try again")
+        })
+    }
+    //delete
+    if(e.target.classList.contains("delete-me")){
+        confirm("do you really want to delete this?")
+        
+        axios.post('/delete-item',{ id:e.target.getAttribute("data-id")}).then(function(){
+            
+            e.target.parentElement.parentElement.remove()
+        }).catch(function(){
+            console.log("error,try again")
+        })
+    }
+})
